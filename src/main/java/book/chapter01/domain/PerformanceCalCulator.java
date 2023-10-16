c85fa6f5aa4a65335ab9b69509707ceb44878ac1
HongJayoung <ghddlwkdud@naver.com>
2023-10-16 20:50:44 +0900

    refactor: 공연료 계산기 만들기

---
+++ src/main/java/book/chapter01/domain/PerformanceCalCulator.java
@@ -0,0 +1,37 @@
+package book.chapter01.domain;
+
+import book.chapter01.dto.Performance;
+import book.chapter01.dto.Play;
+
+public class PerformanceCalCulator {
+    private final Performance performance;
+    private final Play play;
+
+    public PerformanceCalCulator(Performance performance, Play play) {
+        this.performance = performance;
+        this.play = play;
+    }
+
+    public int getAmount() throws Exception {
+        int result = 0;
+
+        switch (play.getType()) {
+            case "tragedy":
+                result = 40000;
+                if (performance.getAudience() > 30) {
+                    result += 1000 * (performance.getAudience() - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (performance.getAudience() > 20) {
+                    result += 10000 + 500 * (performance.getAudience() - 20);
+                }
+                result += 300 * performance.getAudience();
+                break;
+            default:
+                throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
+        }
+        return result;
+    }
+}
