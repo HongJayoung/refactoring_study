7171c3e2a4b5a53432a9fb5ae46fe6403d49d829
HongJayoung <ghddlwkdud@naver.com>
2023-11-20 20:47:44 +0900

    add: 정상 범위를 벗어난 측정값을 찾는 메서드 & 호출문 추가

--- src/main/java/book/chapter06/Application6_8.java
+++ src/main/java/book/chapter06/Application6_8.java
@@ -2,14 +2,22 @@ package book.chapter06;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application6_8 {
     public static void main(String[] args) {
         Station station = createSampleStation();
+        OperationPlan operationPlan = new OperationPlan(50, 55);
+
+        List<Reading> alerts =
+                readingsOutsideRange(
+                        station, operationPlan.getTemperatureFloor(), operationPlan.getTemperatureCeiling());
+        System.out.println("alerts => " + alerts);
     }
 
     private static Station createSampleStation() {
@@ -23,6 +31,12 @@ public class Application6_8 {
                         new Reading(51, LocalDateTime.of(2016, 11, 10, 9, 50)));
 
         return new Station(name, readings);
+    }
+
+    public static List<Reading> readingsOutsideRange(Station station, int min, int max) {
+        return station.getReadings().stream()
+                .filter(r -> r.getTemp() < min || r.getTemp() > max)
+                .collect(Collectors.toList());
     }
 }
 
@@ -38,4 +52,11 @@ class Station {
 class Reading {
     private int temp;
     private LocalDateTime time;
+}
+
+@Getter
+@AllArgsConstructor
+class OperationPlan {
+    private int temperatureFloor;
+    private int temperatureCeiling;
 }
