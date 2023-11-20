cd2f8646a02f884091af57e562246824e8db4b5e
HongJayoung <ghddlwkdud@naver.com>
2023-11-20 20:27:14 +0900

    add: 매개변수 객체만들기(Introduce Parameter Object) 실습 코드 추가

---
+++ src/main/java/book/chapter06/Application6_8.java
@@ -0,0 +1,41 @@
+package book.chapter06;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application6_8 {
+    public static void main(String[] args) {
+        Station station = createSampleStation();
+    }
+
+    private static Station createSampleStation() {
+        String name = "ZB1";
+        List<Reading> readings =
+                List.of(
+                        new Reading(47, LocalDateTime.of(2016, 11, 10, 9, 10)),
+                        new Reading(53, LocalDateTime.of(2016, 11, 10, 9, 20)),
+                        new Reading(58, LocalDateTime.of(2016, 11, 10, 9, 30)),
+                        new Reading(53, LocalDateTime.of(2016, 11, 10, 9, 40)),
+                        new Reading(51, LocalDateTime.of(2016, 11, 10, 9, 50)));
+
+        return new Station(name, readings);
+    }
+}
+
+@Data
+@AllArgsConstructor
+class Station {
+    private String name;
+    private List<Reading> readings = new ArrayList<>();
+}
+
+@Data
+@AllArgsConstructor
+class Reading {
+    private int temp;
+    private LocalDateTime time;
+}
